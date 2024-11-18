f0282e98c42a035121036473d01b1651fd6469ed
Rhodes2716 <Jcone2004@gmail.com>
2024-11-18 16:02:25 -0600

    Added comments for better reading

--- GradeBook/GradeBook.py
+++ GradeBook/GradeBook.py
@@ -1,16 +1,16 @@
 from random import choice
 
-
+# I use a array dictionary to house the arrays, Name of the array is the students and the grades are the values
 gradebook = {'Venni': [75.5, 81.33, 72.50], 'Evan': [79.5, 90.45, 88.50], 'Teezy': [62.81, 58.5, 99.75], 'Cyrus': [70.71, 85.57, 0.0], 'Caden': [96.7, 100.0, 89.95]}
 
-def main():
+def main(): # Constructor function, basically a main menu
     whatdo = input("What would you like to do? \n 1) Create Student \n 2) Enter Grades \n 3) Edit Grades \n 4) View Gradebook \n 5) Remove Grades \n 6) Remove Student \n")
 
-    try:
-        whatdoreturn = int(whatdo)
+    try: # Try this, if it fails return nothing and go back to main.
+        whatdoreturn = int(whatdo) #use int to validate the data is a whole number, if it isn't it throws ValueError
     except  ValueError:
         return
-
+    # Check for value and go to respective function, else just return to main()
     if whatdoreturn == 1:
         create()
     elif whatdoreturn == 2:
@@ -31,7 +31,7 @@ def enter(): # Enter a grade
     global gradebook
     student_name = input("Student Name: ")
 
-    if student_name == "":
+    if student_name == "": # Return to main if nothing is entered.
         main()
         return
 
@@ -69,7 +69,7 @@ def create(): # Create Student
         print(f"Student '{student_name}' added.")
     else:
         i = int(input("How many: "))
-        for i in range(i): # uses i for iteratives -1 because of 0
+        for i in range(i): # uses i for iteratives
             student_name = input("Name: ")
             gradebook[student_name] = []
             print(f"Student '{student_name}' added.")
@@ -86,10 +86,9 @@ def edit(): # Edit Grades
         
     else:
         print("Name not in system.")
-        return
     main()
 
-def removegrade ():
+def removegrade (): # Remove a grade
     global gradebook
     try:
         student_name = str(input("Which Student: "))
@@ -100,7 +99,7 @@ def removegrade ():
         print("Invalid input.")
     main()
 
-def removestudent():
+def removestudent(): # Remove a student
     global gradebook
     bulk_question = int(input("1) One Student \n2) Many Students \n"))
     try:
